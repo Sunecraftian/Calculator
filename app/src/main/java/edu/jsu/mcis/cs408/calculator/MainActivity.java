f0d3f898872576d87f72aa6eca162f9f5b964adb
Suneet Sharma <sunecraftian@gmail.com>
2024-02-13 14:19:03 -0600

    Added dynamic display TextView

--- app/src/main/java/edu/jsu/mcis/cs408/calculator/MainActivity.java
+++ app/src/main/java/edu/jsu/mcis/cs408/calculator/MainActivity.java
@@ -1,15 +1,27 @@
 package edu.jsu.mcis.cs408.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import edu.jsu.mcis.cs408.calculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    private ConstraintLayout layout;
+    private ConstraintSet set;
+    private LayoutParams params;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +30,40 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
 
         setContentView(view);
+
+        initLayout();
+    }
+
+    private void initLayout() {
+        layout = binding.layout;
+        set = new ConstraintSet();
+
+        TextView display = new TextView(this);
+        display.setId(View.generateViewId());
+        display.setTag("display");
+        display.setGravity(Gravity.END);
+        display.setText("0");
+        display.setTextSize(48);
+        display.setBackgroundColor(Color.CYAN);
+
+
+
+        layout.addView(display);
+
+        set.clone(layout);
+
+        set.connect(display.getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, 0);
+        set.connect(display.getId(), ConstraintSet.RIGHT, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT, 0);
+        set.connect(display.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0);
+
+
+
+        set.applyTo(layout);
+
+        params = display.getLayoutParams();
+        params.width = ConstraintLayout.LayoutParams.MATCH_CONSTRAINT;
+        params.height = ConstraintLayout.LayoutParams.WRAP_CONTENT;
+        display.setLayoutParams(params);
+
     }
 }
